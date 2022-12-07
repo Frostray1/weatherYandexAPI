29339c83b0cdc3259ad94982410c6d1a2cf9a140
Artem <a.bychkoff1@yandex.ru>
2022-12-07 23:22:16 +0300

    добавлено изменение фона при различных погодных условиях

--- js/changeBackgroundScript.js
+++ js/changeBackgroundScript.js
@@ -1,12 +1,10 @@
 export function changeBackground (condition){
     const status = {
-       
-        "overcast": "url('../../img/weather/As-Above-So-Below-2560x1600.jpg')"
-        // "overcast": "url('../../img/weather/RooftopClouds.mp4')"
+        "overcast": "/img/weather/RooftopClouds.mp4"
       }
     const color = {
       "overcast": '#65747f'
     }
       document.getElementById('weather').style.backgroundColor = color[condition]
-      return document.body.style.background = `${status[condition]}`;
+      return document.getElementById('bgVideo__media').src = `${status[condition]}`
 }
