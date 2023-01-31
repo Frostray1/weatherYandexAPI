1881b93049112b0ef5e7ad42883ba2f7362ac025
Artem <a.bychkoff1@yandex.ru>
2023-01-31 18:10:50 +0300

    add preloader

--- js/preloaderScript.js
+++ js/preloaderScript.js
@@ -1,18 +1,18 @@
-// window.onload = function () {
-//     const preload = document.querySelector(".preloader-5");
-//     const preload__text = document.querySelector(".preloader__text");
-//     const preload_window = document.querySelector(".preloader__window");
-//     setTimeout(function () {
-//       preload__text.textContent = "Настраиваемся на хорошую погоду";
-//     }, 1000);
-//     setTimeout(function () {
-//       preload__text.textContent = "Разгоняем тучи...";
-//     }, 2000);
-//     setTimeout(function () {
-//       preload__text.textContent = "Приготовились!";
-//     }, 3000);
-//     setTimeout(function () {
-//       preload.classList.add("preloader-remove");
-//       preload_window.classList.add("preloader-remove");
-//     }, 4000);
-//   };
+window.onload = function () {
+    const preload = document.querySelector(".preloader-5");
+    const preload__text = document.querySelector(".preloader__text");
+    const preload_window = document.querySelector(".preloader__window");
+    setTimeout(function () {
+      preload__text.textContent = "Настраиваемся на хорошую погоду";
+    }, 1000);
+    setTimeout(function () {
+      preload__text.textContent = "Разгоняем тучи...";
+    }, 2000);
+    setTimeout(function () {
+      preload__text.textContent = "Приготовились!";
+    }, 3000);
+    setTimeout(function () {
+      preload.classList.add("preloader-remove");
+      preload_window.classList.add("preloader-remove");
+    }, 4000);
+  };
